cef50841449788a0b55413ed1d49c8b19f0ab01e
Sarah Benkoussa <sb967@cornell.edu>
2024-03-22 05:44:08 -0400

    tmp version of search working (not boolean yet)

--- backend/app.py
+++ backend/app.py
@@ -4,9 +4,16 @@ from flask import Flask, render_template, request
 from flask_cors import CORS
 from helpers.MySQLDatabaseHandler import MySQLDatabaseHandler
 import pandas as pd
+
+###########################################################
+###########################################################
+# TODO: fix boolean search attempt:
+from helpers.data_analysis import filler_words, build_inverted_index
 from collections import defaultdict, Counter
 from nltk.tokenize import TreebankWordTokenizer
-from helpers.data_analysis import *
+###########################################################
+###########################################################
+
 
 # ROOT_PATH for linking with all your files. 
 # Feel free to use a config.py or settings.py with a global export variable
@@ -16,57 +23,111 @@ os.environ['ROOT_PATH'] = os.path.abspath(os.path.join("..",os.curdir))
 current_directory = os.path.dirname(os.path.abspath(__file__))
 
 # Specify the path to the JSON file relative to the current script
-json_file_path = os.path.join(current_directory, 'data.json')
+json_file_path = os.path.join(current_directory, 'init.json')
 
 # Assuming your JSON data is stored in a file named 'init.json'
 with open(json_file_path, 'r') as file:
-    data = json.load(file)
-    episodes_df = pd.DataFrame(data['episodes'])
-    reviews_df = pd.DataFrame(data['reviews'])
+    data = json.load(file) # nested dict same format as json
+
+    gname, gphone, gweb, grat, gdesc, gaddr, gnumrev, grev = zip(*((
+        gym['name'],
+        gym['phone'],
+        gym['website'],
+        gym['rating'],
+        gym['description'],
+        gym['address'],
+        gym['num_online_reviews'],
+        gym['reviews']
+    ) for gym in data.values()))
+
+    data_df =  pd.DataFrame({
+        'name': gname,
+        'phone': gphone,
+        'website': gweb,
+        'rating': grat,
+        'description': gdesc,
+        'address': gaddr,
+        'num_online_reviews': gnumrev,
+        'reviews': grev
+    })
 
 app = Flask(__name__)
 CORS(app)
 
-inverted_index = build_inverted_index(data)
-# Sample search using json with pandas
+
+# TMP version, sample search using json with pandas
 def json_search(query):
+    """
+    returns: JSON of gyms that contain exact query string in description (not reviews)
+    """
     matches = []
-    merged_df = pd.merge(episodes_df, reviews_df, left_on='id', right_on='id', how='inner')
-    matches = merged_df[merged_df['title'].str.lower().str.contains(query.lower())]
-    matches_filtered = matches[['title', 'descr', 'imdb_rating']]
-    matches_filtered_json = matches_filtered.to_json(orient='records')
+    merged_df = data_df
+    matches = merged_df[merged_df['description'].str.lower().str.contains(query.lower())]
+    matches_filtered = matches[['name', 'description', 'rating', 'website']]
+    matches_filtered_json = matches_filtered.to_json(orient='records') # fxn to_json() only works with a pandas dataframe
     return matches_filtered_json
 
+
+
+###########################################################
+###########################################################
+# TODO: fix boolean search attempt:
+
+inverted_index = build_inverted_index(data)
+
 def boolean_search(query:str, inverted_index:dict):
+    """
+    returns: JSON of gyms filtered by boolean search on their reviews
+
+    Note: data MUST be a pandas dataframe to use to_json() needed to return output (see variable data_df)
+    """
+    # TODO: edit and debug to fit spec and return same format as json_search()
     res = set()
     if not query:
-      return res
+      print("if not query returns res = ",res)
+      return list(res)
     full_query_tokens = set(TreebankWordTokenizer().tokenize(query))
 
     query_tokens=[]
     for token in full_query_tokens:
-      if token not in filler_words:
+      if token not in filler_words(data):
         query_tokens.append(token)
 
     res=inverted_index.get(query_tokens[0])
     for token in query_tokens:
         res = res.intersection(inverted_index.get(token, set()))
-    return res
+    print("typed query returns res = ",res)
+    return list(res)
+###########################################################
+###########################################################
+
+
+
 
 
 @app.route("/")
 def home():
     return render_template('base.html',title="sample html")
 
-@app.route("/episodes")
-def episodes_search():
-    text = request.args.get("title")
-    return json_search(text)
-
+# TMP version:
 @app.route("/gyms")
 def gym_search():
    text = request.args.get("query")
-   return boolean_search(text, inverted_index).to_json(orient='records')
+   return json_search(text)
+
+
+###########################################################
+###########################################################
+# TODO: fix boolean search attempt:
+# uncomment and fix, (comment out the TMP version):
+
+# @app.route("/gyms")
+# def gym_search():
+#    text = request.args.get("query")
+#    return boolean_search(text, inverted_index).to_json(orient='records')
+
+###########################################################
+###########################################################
 
 
 if 'DB_NAME' not in os.environ:
